3735ce9803e4fda744d5e1bfdb1428ec85a73dc6
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2021-12-05 13:24:33 +0200

    Add project files.

---
+++ Data_input._Arithmetic_operators/task_1.cpp
@@ -0,0 +1,11 @@
+//Пользователь вводит число. Программа вычисляет 3-ю степень этого числа.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "" << endl;
+	
+	return 0;
+}
---
+++ task_4/task_4.cpp
@@ -0,0 +1,11 @@
+//Написать программу, которая находит процент P от суммы S.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "" << endl;
+
+	return 0;
+}
